d9910adb05e05838238407f4b7f9ee8691ef1a36
familiadelafuente1942-afk <familiadelafuente1942@gmail.com>
2026-06-30 15:08:48 -0300

    Add files via upload

---
+++ main-cliente.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './global.css'
+import ClienteApp from './Cliente.jsx'
+createRoot(document.getElementById('root')).render(<ClienteApp />)
