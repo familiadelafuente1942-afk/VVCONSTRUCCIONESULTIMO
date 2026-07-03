a51fdf92f83a53294816d32d746e9f9b5849bcbd
familiadelafuente1942-afk <familiadelafuente1942@gmail.com>
2026-07-03 18:25:53 -0300

    Add files via upload

---
+++ main-miasistente 2.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './global.css'
+import MiAsistente from './MiAsistente.jsx'
+createRoot(document.getElementById('root')).render(<MiAsistente />)
