2f9a2b11da252bcf70395a2fd932eb159e5ec549
familiadelafuente1942-afk <familiadelafuente1942@gmail.com>
2026-07-14 11:49:42 -0300

    Create main-cronograma.jsx

---
+++ main-cronograma.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './global.css'
+import Cronograma from './Cronograma.jsx'
+createRoot(document.getElementById('root')).render(<Cronograma />)
