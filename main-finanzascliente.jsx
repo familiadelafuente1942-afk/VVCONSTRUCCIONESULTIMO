54e8b1db1c8887764b19d97fb4455720f5b687c6
familiadelafuente1942-afk <familiadelafuente1942@gmail.com>
2026-08-16 12:19:42 -0300

    Add files via upload

---
+++ main-finanzascliente.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './global.css'
+import FinanzasCliente from './FinanzasCliente.jsx'
+createRoot(document.getElementById('root')).render(<FinanzasCliente />)
